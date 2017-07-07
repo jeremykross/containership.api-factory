2f0c206e15c868a54de12080d4d702e15c0d1907
Jeremy Kross <jeremy.kross@containership.io>
2017-07-07 17:48:11 +0000

    Fixed package names.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const ContainershipApi = require('containership.api-bridge');
-const KubernetesApi = require('containership.k8s.api-bridge');
+const ContainershipApi = require('@containership/containership.api-bridge');
+const KubernetesApi = require('@containership/containership.k8s.api-bridge');
 
 const Constructor = process.env.CS_ORCHESTRATOR === 'kubernetes' ? KubernetesApi : ContainershipApi;
 const DEFAULT_PORT = 8080;
